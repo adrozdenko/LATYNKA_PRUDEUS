27083cdb4578a9ff2144d90277b00f1be5d7be14
Andrii Drozdenko <adrozdenko@gmail.com>
2022-12-24 19:33:44 +0200

    added core functionality

---
+++ script.js
@@ -0,0 +1,24 @@
+import translitEngine from 'translitit-engine';
+import {table} from './lib/translitit-cyrillic-ukrainian-to-latin-prudeus'
+const inputForm = document.querySelector('#input_form');
+const outputForm = document.querySelector('#output_form');
+const transliterate = translitEngine(table)
+
+inputForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const inputText = e.target.querySelector('#input_text').value
+    const transcription = transliterate(inputText)
+    outputForm.querySelector('#output_text').value = transcription
+})
+
+outputForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const copyTextarea = event.target.querySelector("#output_text");
+    copyTextarea.focus();
+    copyTextarea.select();
+    navigator.clipboard.writeText(copyTextarea.value);
+  });
+
+
+
+
